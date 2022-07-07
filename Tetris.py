87418a7c1569eb8efa8fdc8c57722dfb12cdaf64
Daniel Zombek <djzombek@gmail.com>
2022-07-06 20:40:30 -0400

    Add icon.png and code to set icon

--- Tetris.py
+++ Tetris.py
@@ -11,13 +11,15 @@ w_height = 800
 #screen initialization
 screen = pygame.display.set_mode((w_width, w_height))
 pygame.mouse.set_visible(0)
-
+pygame.display.set_caption('Tetris')
+programIcon = pygame.image.load('icon.png')
+pygame.display.set_icon(programIcon)
 #main loop
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
-
+			sys.exit(0)
 
 
 
