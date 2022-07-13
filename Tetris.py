ccaca8105e36f9fb28ba893ba25952b01456eea3
Daniel Zombek <djzombek@gmail.com>
2022-07-13 17:57:43 -0400

    Added grid lines that show up after pressing the start button.
    Sorted initialization commands into relevant functions.

--- Tetris.py
+++ Tetris.py
@@ -14,36 +14,39 @@ w_height = 800
 screen = pygame.display.set_mode((w_width, w_height))
 pygame.mouse.set_visible(True)
 pygame.display.set_caption('Tetris')
-programIcon = pygame.image.load('icon.png')
-pygame.display.set_icon(programIcon)
+program_icon = pygame.image.load('icon.png')
+pygame.display.set_icon(program_icon)
 
-# main menu
-# start game button
-start_button = pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(320, 350, 160, 100))
 
-# quit game button
-quit_button = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(320, 550, 160, 100))
+#
+# NEED TO ADD ACTUAL DIMENSIONS OF PLAYING FIELD AND GRID SIZE
+#
+def draw_grid():
+    # gets distance between lines
+    block_size = 30
+    # gets x position
+    x = 220
+    # gets y position
+    y = 70
 
-# text on buttons
-font = pygame.font.SysFont('Arial', 30)
-start_text = font.render('Start', False, (0, 0, 0))
-start_rect = start_text.get_rect(center=start_button.center)
-screen.blit(start_text, start_rect)
-quit_text = font.render('Quit', False, (0, 0, 0))
-quit_rect = quit_text.get_rect(center=quit_button.center)
-screen.blit(quit_text, quit_rect)
+    for _ in range(11):
+        # moves x and y every iteration one block
+        x = x + block_size
 
-# add everything to screen
-pygame.mouse.set_visible(True)
-pygame.display.flip()
+        # adds a white line every block over on the x axis across whole screen
+        pygame.draw.line(screen, (255, 255, 255), (x, 100), (x, w_width - 100))
 
-# add menu music
-# pygame.mixer.init()
-# pygame.mixer.music.load('birthofahero.wav')
-# pygame.mixer.music.play(-1)
+    for _ in range(21):
+        y = y + block_size
+        # adds a white line every block up on the y axis across whole screen
 
+        pygame.draw.line(screen, (255, 255, 255), (250, y), (w_width - 250, y))
 
 def game():
+    # change screen color
+    screen.fill((0, 0, 0))
+    draw_grid()
+    pygame.display.flip()
     # pygame.mixer.music.stop()
     # pygame.mixer.music.load('endlessmotion.wav')
     # pygame.mixer.music.play(-1)
@@ -61,9 +64,32 @@ def game():
                 sys.exit(0)
 
 
-# more code stuff
-
 def main_menu():
+    # main menu
+    # start game button
+    start_button = pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(320, 350, 160, 100))
+
+    # quit game button
+    quit_button = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(320, 550, 160, 100))
+
+    # text on buttons
+    font = pygame.font.SysFont('Arial', 30)
+    start_text = font.render('Start', False, (0, 0, 0))
+    start_rect = start_text.get_rect(center=start_button.center)
+    screen.blit(start_text, start_rect)
+    quit_text = font.render('Quit', False, (0, 0, 0))
+    quit_rect = quit_text.get_rect(center=quit_button.center)
+    screen.blit(quit_text, quit_rect)
+
+    # add everything to screen
+    pygame.mouse.set_visible(True)
+    pygame.display.flip()
+
+    # add menu music
+    # pygame.mixer.init()
+    # pygame.mixer.music.load('birthofahero.wav')
+    # pygame.mixer.music.play(-1)
+
     while True:
         for event in pygame.event.get():
             # on event click
@@ -71,9 +97,6 @@ def main_menu():
                 # get mouse position
                 mouse = pygame.mouse.get_pos()
                 if 320 <= mouse[0] <= 480 and 350 <= mouse[1] <= 450:
-                    # change screen color
-                    screen.fill((255, 255, 255))
-                    pygame.display.flip()
                     game()
                 elif 320 <= mouse[0] <= 480 and 550 <= mouse[1] <= 650:
                     pygame.quit()
@@ -84,6 +107,3 @@ def main_menu():
 
 
 main_menu()
-# def game():
-#     while True:
-# stuff
