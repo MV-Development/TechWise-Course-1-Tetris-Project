2fd796ab2ef69c20cba71375ce3fd9e089d95491
Daniel Zombek <djzombek@gmail.com>
2022-07-13 15:01:03 -0400

    Added text 'Start' and 'Quit' to main menu buttons.
    Disabled music.
    
    Formatted code.

--- Tetris.py
+++ Tetris.py
@@ -4,6 +4,7 @@ import sys
 import os
 
 pygame.init()
+pygame.font.init()
 
 # window variables
 w_width = 800
@@ -19,19 +20,34 @@ pygame.display.set_icon(programIcon)
 # main menu
 # start game button
 start_button = pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(320, 350, 160, 100))
+
 # quit game button
 quit_button = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(320, 550, 160, 100))
+
+# text on buttons
+font = pygame.font.SysFont('Arial', 30)
+start_text = font.render('Start', False, (0, 0, 0))
+start_rect = start_text.get_rect(center=start_button.center)
+screen.blit(start_text, start_rect)
+quit_text = font.render('Quit', False, (0, 0, 0))
+quit_rect = quit_text.get_rect(center=quit_button.center)
+screen.blit(quit_text, quit_rect)
+
+# add everything to screen
 pygame.mouse.set_visible(True)
 pygame.display.flip()
-pygame.mixer.init()
-pygame.mixer.music.load('birthofahero.wav')
-pygame.mixer.music.play(-1)
+
+# add menu music
+# pygame.mixer.init()
+# pygame.mixer.music.load('birthofahero.wav')
+# pygame.mixer.music.play(-1)
 
 
 def game():
-    pygame.mixer.music.stop()
-    pygame.mixer.music.load('endlessmotion.wav')
-    pygame.mixer.music.play(-1)
+    # pygame.mixer.music.stop()
+    # pygame.mixer.music.load('endlessmotion.wav')
+    # pygame.mixer.music.play(-1)
+
     pygame.mouse.set_visible(False)
     while True:
         for event in pygame.event.get():
