08fd72356f68a0da4f12f98620b5e87da4688704
Daniel Zombek <djzombek@gmail.com>
2022-07-20 18:58:13 -0400

    Added minutes to timer

--- hud.py
+++ hud.py
@@ -7,13 +7,16 @@ def create_hud(screen, start_time):
     passed_time = 0
     game_clock = pygame.time.Clock()
     player_score = 0
-    font = pygame.font.Font(None, 20)
+    font = pygame.font.Font(None, 30)
     font_color = pygame.Color('white')
     passed_time = pygame.time.get_ticks() - start_time
-    sec_str = str((passed_time%60000)/1000).zfill(2)
-    text = font.render(str(sec_str), True, font_color)
+    min_str = str(passed_time//60000).zfill(2)
+    sec_str = str((passed_time%60000)//1000).zfill(2)
+    mill_str = str(passed_time%1000).zfill(3)
+    score_str = ('{min}:{sec}:{mil}'.format(min = min_str, sec = sec_str, mil = mill_str))
+    text = font.render(str(score_str), True, font_color)
     screen.fill((0,0,0), (0, 0, 800, 100))
     screen.blit(text, (50, 50))
     game_clock.tick(25)
-    scoreDisplay = font.render("CURRENT SCORE: ", True, font_color)  # ADD SCORE FUNCTION
-    screen.blit(scoreDisplay, (600, 50))
+    score_display = font.render("CURRENT SCORE: ", True, font_color)  # ADD SCORE FUNCTION
+    screen.blit(score_display, (300, 50))
