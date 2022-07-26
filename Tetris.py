f863dc046cb6f63556dc1342e0aea981705e3a0f
brian <bruane>
2022-07-25 19:55:21 -0500

    Time Limit Modes, GAME OVER screen, basic font increases

--- Tetris.py
+++ Tetris.py
@@ -92,7 +92,6 @@ def new_piece(color):
 def sort_blocks():
     print("blocks sorted")
 
-
 def game():
     # change screen color
     screen.fill(BLACK)
@@ -110,7 +109,88 @@ def game():
     update_grid(piece_grid)
 
     while True:
-        hud.create_hud(screen, start_time)  ###ATTEMPT AT GAME CLOCK
+        hud.create_hud(screen, start_time)
+        ###ATTEMPT AT GAME CLOCK
+        pygame.display.update()
+        for event in pygame.event.get():
+            # spacebar quits game
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pygame.quit()
+                    sys.exit(0)
+                if event.key == pygame.K_RIGHT:
+                    for row in range(len(grid)):
+                        for col in range(len(grid[row]) - 1):
+                            if grid[row][col + 1] == BLACK and piece_grid[row][col] == color:
+                                piece_grid[row][col] = BLACK
+                                piece_grid[row][col + 1] = color
+                                break
+                if event.key == pygame.K_LEFT:
+                    for row in range(len(grid)):
+                        for col in range(len(grid[row])):
+                            if grid[row][col - 1] == BLACK and piece_grid[row][col] == color:
+                                piece_grid[row][col] = BLACK
+                                piece_grid[row][col - 1] = color
+                                break
+
+                update_grid(piece_grid)
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+def game_over():
+    screen.fill(BLACK)
+    font = pygame.font.SysFont('Arial', 60)
+    game_over_space = pygame.draw.rect(screen, BLACK, pygame.Rect(320, 250, 160, 100))
+    game_over_text = font.render('GAME OVER', False, WHITE)
+    game_over_rect = game_over_text.get_rect(center=game_over_space.center)
+    screen.blit(game_over_text, game_over_rect)
+
+    quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
+    quit_text = font.render('Quit', False, BLACK)
+    quit_rect = quit_text.get_rect(center=quit_button.center)
+    screen.blit(quit_text, quit_rect)
+
+    pygame.mouse.set_visible(True)
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            # on event click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # get mouse position
+                mouse = pygame.mouse.get_pos()
+                if 320 <= mouse[0] <= 480 and 350 <= mouse[1] <= 450:
+                    game_select()
+                elif 320 <= mouse[0] <= 480 and 550 <= mouse[1] <= 650:
+                    pygame.quit()
+                    sys.exit(0)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+def game5():
+    # change screen color
+    screen.fill(BLACK)
+    # draw_grid()
+    # pygame.mixer.music.stop()
+    # pygame.mixer.music.load('endlessmotion.wav')
+    # pygame.mixer.music.play(-1)
+    # new_piece()
+    draw_lines()
+    pygame.mouse.set_visible(False)
+    start_time = pygame.time.get_ticks()
+    color = random.choice(COLORS)
+    grid = create_grid()
+    piece_grid = new_piece(color)
+    update_grid(piece_grid)
+    clock = pygame.time.Clock
+    while True:
+        theTime = hud.limit5(screen, start_time)
+        if theTime == "ASSY":
+            game_over()
+        ###ATTEMPT AT GAME CLOCK
         pygame.display.update()
         for event in pygame.event.get():
             # spacebar quits game
@@ -149,7 +229,7 @@ def main_menu():
     quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
 
     # text on buttons
-    font = pygame.font.SysFont('Arial', 30)
+    font = pygame.font.SysFont('Arial', 50)
     start_text = font.render('Start', False, BLACK)
     start_rect = start_text.get_rect(center=start_button.center)
     screen.blit(start_text, start_rect)
@@ -173,6 +253,55 @@ def main_menu():
                 # get mouse position
                 mouse = pygame.mouse.get_pos()
                 if 320 <= mouse[0] <= 480 and 350 <= mouse[1] <= 450:
+                    game_select()
+                elif 320 <= mouse[0] <= 480 and 550 <= mouse[1] <= 650:
+                    pygame.quit()
+                    sys.exit(0)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+def game_select():
+    screen.fill(BLACK)
+    # main menu
+    # start game button
+
+    limit5_button = pygame.draw.rect(screen, GREEN, pygame.Rect(320, 250, 160, 100))
+    limit10_button = pygame.draw.rect(screen, BLUE, pygame.Rect(320, 350, 160, 100))
+    limit15_button = pygame.draw.rect(screen, GREEN, pygame.Rect(320, 450, 160, 100))
+
+    # quit game button
+    quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
+
+    # text on buttons
+    font = pygame.font.SysFont('Arial', 30)
+    font_title = pygame.font.SysFont('Arial', 60)
+
+    limit5_text = font.render('5 Min Limit', False, BLACK)
+    limit10_text = font.render('10 Min Limit', False, BLACK)
+    limit15_text = font.render('15 Min Limit', False, BLACK)
+    limit5_rect = limit5_text.get_rect(center=limit5_button.center)
+    limit10_rect = limit10_text.get_rect(center=limit10_button.center)
+    limit15_rect = limit15_text.get_rect(center=limit15_button.center)
+    screen.blit(limit5_text, limit5_rect)
+    screen.blit(limit10_text, limit10_rect)
+    screen.blit(limit15_text, limit15_rect)
+    quit_text = font.render('Quit', False, BLACK)
+    quit_rect = quit_text.get_rect(center=quit_button.center)
+    screen.blit(quit_text, quit_rect)
+    pygame.mouse.set_visible(True)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            # on event click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # get mouse position
+                mouse = pygame.mouse.get_pos()
+                if 320 <= mouse[0] <= 480 and 350 <= mouse[1] <= 450:
+                    game()
+                elif 320 <= mouse[0] <= 480 and 250 <= mouse[1] <= 350:
+                    game5()
+                elif 320 <= mouse[0] <= 480 and 450 <= mouse[1] <= 550:
                     game()
                 elif 320 <= mouse[0] <= 480 and 550 <= mouse[1] <= 650:
                     pygame.quit()
@@ -182,7 +311,10 @@ def main_menu():
                 sys.exit(0)
 
 
+
 main_menu()
+
+
 
 # 4x4 grid
 # 0   1    2    3
