26b77894c5fc1b3a138a1242a4f521d6bd15dfef
Mike V <mikevisa4500x@gmail.com>
2022-07-31 23:15:39 -0400

    Added Piece classes in pieces.py, may be of use -Mike

--- pieces.py
+++ pieces.py
@@ -7,44 +7,78 @@ O = [['xxxxx'
 piece_names = (O)
 
 
-class Piece:
-    def __init__(self, x, y, tetro):
-        self.x = x
-        self.y = y
-        self.tetro = tetro
-        self.color = random.choice(COLORS)
-        self.rotation = 0
+#class piece
+#   def __init__(self, x, y, tetro):
+#      self.x = x
+#       self.y = y
+#       self.tetro = tetro
+#       self.color = random.choice(COLORS)
+#       self.rotation = 0
 
 
 # Orange Ricky
 #   0
 # 000
-
+class OrangeRicky:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x + 2, y), (x, y + 1), (x + 1, y + 1), (x + 2, y + 1))
 
 # Blue Ricky
 # 0
 # 000
+class BlueRicky:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x, y), (x, y + 1), (x + 1, y + 1), (x + 2, y + 1))
 
 
 # Cleveland Z
 # 00
 #  00
+class CleavelandZ:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x, y), (x + 1, y), (x + 1, y + 1), (x + 2, y + 1))
 
 
 # Rhode Island Z
 #  00
 # 00
+class RhodeIslandZ:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x, y + 1), (x + 1, y + 1), (x + 1, y), (x + 2, y))
 
 
 # Hero
 # 0000
+class Her0:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x, y), (x + 1, y), (x + 2, y), (x + 3, y))
 
 
 # Teewee
 #  0
 # 000
+class Teewee:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x + 1, y), (x, y + 1), (x + 1, y + 1), (x + 2, y + 1))
 
 
 # Smashboy
 # 00
 # 00
+class SmashBoy:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blocks = ((x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1))
