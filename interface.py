3bef96a871d09102195427e8b7d8433ade5d4ca4
Alpha <mikevisa4500x@gmail.com>
2022-08-08 21:08:15 -0400

    menu button class is 99.99% working
    Signed-off-by: Alpha <mikevisa4500x@gmail.com>

--- interface.py
+++ interface.py
@@ -1,11 +1,9 @@
 ########################################################################################################################
 # Import statements
 import pygame
-from pygame import font
 
 ########################################################################################################################
 # Colors and Corresponding RGB Values
-
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0),
@@ -14,35 +12,29 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 
-# limit5_button = pygame.draw.rect(screen, colors.RED, pygame.Rect(320, 250, 160, 100))
-
-# limit5_text = pygame.font.render('5 Min Limit', False, colors.BLACK)
-
-
-# limit5_rect = limit5_text.get_rect(center=limit5_button.center)
-
-
 ########################################################################################################################
 # Menu Button Class
 class MenuButton:
     # Constructor Method
-    def __init__(self, surface, color, start_x, start_y, display_size, text, text_font, text_size):
+    def __init__(self, surface, background_color, start_x, start_y, display_size, text, text_color, text_font,
+                 text_size):
         self.surface = surface
-        self.color = color
+        self.color = background_color
         self.start_x = start_x
         self.start_y = start_y
         self.length = display_size * 0.2
         self.width = display_size * 0.125
         self.text = text
+        self.text_color = text_color
         self.text_style = pygame.font.SysFont(text_font, text_size)
 
     def draw_button(self):
         background = pygame.draw.rect(self.surface, self.color,
                                       pygame.Rect(self.start_x, self.start_y, self.length, self.width))
         text_font = self.text_style
-        text = text_font.render(self.text, False, self.color)
+        text = text_font.render(self.text, False, self.text_color)
         button = text.get_rect(center=background.center)
-        self.surface.blit(self.surface, background)
+        self.surface.blit(text, background)
 
     def is_clicked(self):
         mouse = pygame.mouse.get_pos()
--- main_menu.py
+++ main_menu.py
@@ -14,33 +14,27 @@ SIZE = block_game.SIZE
 
 ########################################################################################################################
 # main menu
-tetris_button = pygame.draw.rect(SCREEN, interface.BLACK, pygame.Rect(320, 130, 160, 100))
-tetris_font = pygame.font.SysFont('javanesetext', 100)
-tetris_text = tetris_font.render('Block Game', False, interface.WHITE)
-tetris_rect = tetris_text.get_rect(center=tetris_button.center)
-SCREEN.blit(tetris_text, tetris_rect)
+# tetris_button = pygame.draw.rect(SCREEN, interface.BLACK,
+#                                 pygame.Rect(SIZE * 0.4, SIZE * 0.15, SIZE * 0.2, SIZE * 0.125))
+# tetris_font = pygame.font.SysFont('javanesetext', 100)
+# tetris_text = tetris_font.render('Block Game', False, interface.WHITE)
+# tetris_rect = tetris_text.get_rect(center=tetris_button.center)
+# SCREEN.blit(tetris_text, tetris_rect)
 
-# tetris_button = interface.MenuButton(SCREEN, interface.WHITE, SIZE * (1/8), SIZE * (1/8), SIZE, 'BLOCK GAME', 'javanesetext', 100)
-# tetris_button.draw_button()
+tetris_button = interface.MenuButton(SCREEN, interface.BLACK, SIZE * 0.4, SIZE * 0.15, SIZE, 'BLOCK GAME',
+                                     interface.WHITE, 'javanesetext', 100)
+tetris_button.draw_button()
 
+font = pygame.font.SysFont('franklingothicmedium', 50)
 # start game button
-start_button = pygame.draw.rect(SCREEN, (118, 238, 198),
-                                pygame.Rect(SIZE * 0.1475, SIZE * 0.4375, SIZE * 0.2, SIZE * 0.125))
-start_text = font.render('Start', False, interface.BLACK)
-start_rect = start_text.get_rect(center=start_button.center)
-SCREEN.blit(start_text, start_rect)
-
-# start_button = interface.MenuButton(SCREEN, interface.GREEN, SIZE * (3/8), SIZE * (3/8), SIZE, 'START', 'franklingothicmedium', 50)
-# start_button.draw_button()
+start_button = interface.MenuButton(SCREEN, interface.GREEN, SIZE * (320 / 800), SIZE * (350 / 800), SIZE, 'START',
+                                    interface.WHITE, 'franklingothicmedium', 50)
+start_button.draw_button()
 
 # quit game button
-quit_button = pygame.draw.rect(SCREEN, interface.RED, pygame.Rect(SIZE * 0.4, SIZE * 0.6875, SIZE * 0.2, SIZE * 0.125))
-quit_text = font.render('Quit', False, interface.BLACK)
-quit_rect = quit_text.get_rect(center=quit_button.center)
-SCREEN.blit(quit_text, quit_rect)
-
-# quit_button = interface.MenuButton(SCREEN, interface.RED, SIZE * (5/8), SIZE * (5/8), SIZE, 'QUIT', 'franklingothicmedium', 50)
-# quit_button.draw_button()
+quit_button = interface.MenuButton(SCREEN, interface.RED, SIZE * (320 / 800), SIZE * (550 / 800), SIZE, 'QUIT',
+                                   interface.BLACK, 'franklingothicmedium', 50)
+quit_button.draw_button()
 
 # add everything to SCREEN
 
@@ -51,7 +45,7 @@ while True:
     for event in pygame.event.get():
         # on event click
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print("mouse clicked")
+            print("mouse clicked", pygame.mouse.get_pos())
             if start_button.is_clicked():
                 print("start_button clicked")
                 select_screen.difficulty_select()
