f0cdecfd1472346ea86c3c8aec566a475558c9d7
Vaishnavi Singari <ssvd02@gmail.com>
2022-08-10 12:49:17 -0400

    Added animated blocks that rotate using sprites to menu and game_select functions

--- main_menu.py
+++ main_menu.py
@@ -14,11 +14,115 @@ BLOCK_SIZE = 30
 w_width = 800
 w_height = 800
 
+# All Images: Tetris by Emily L from <a href="https://thenounproject.com/browse/icons/term/tetris/" target="_blank" title="Tetris Icons">Noun Project</a>
+
+class LShapeSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(LShapeSprite, self).__init__()
+        self.images = []
+        self.images.append(pygame.image.load('Images/Lshape.png'))
+        self.images.append(pygame.image.load('Images/Lshape1.png'))
+        self.images.append(pygame.image.load('Images/Lshape2.png'))
+        self.images.append(pygame.image.load('Images/Lshape3.png'))
+        self.images.append(pygame.image.load('Images/Lshape4.png'))
+        self.images.append(pygame.image.load('Images/Lshape5.png'))
+        self.images.append(pygame.image.load('Images/Lshape6.png'))
+        self.images.append(pygame.image.load('Images/Lshape7.png'))
+        self.images.append(pygame.image.load('Images/Lshape8.png'))
+        self.images.append(pygame.image.load('Images/Lshape9.png'))
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(500, 500, 100, 100)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+class ZShapeSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(ZShapeSprite, self).__init__()
+        self.images = []
+        self.images.append(pygame.image.load('Images/Zshape.png'))
+        self.images.append(pygame.image.load('Images/Zshape1.png'))
+        self.images.append(pygame.image.load('Images/Zshape2.png'))
+        self.images.append(pygame.image.load('Images/Zshape3.png'))
+        self.images.append(pygame.image.load('Images/Zshape4.png'))
+        self.images.append(pygame.image.load('Images/Zshape5.png'))
+        self.images.append(pygame.image.load('Images/Zshape6.png'))
+        self.images.append(pygame.image.load('Images/Zshape7.png'))
+        self.images.append(pygame.image.load('Images/Zshape8.png'))
+        self.images.append(pygame.image.load('Images/Zshape8.png'))
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(500, 200, 100, 100)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+class TShapeSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(TShapeSprite, self).__init__()
+        self.images = []
+        self.images.append(pygame.image.load('Images/Tshape.png'))
+        self.images.append(pygame.image.load('Images/Tshape1.png'))
+        self.images.append(pygame.image.load('Images/Tshape2.png'))
+        self.images.append(pygame.image.load('Images/Tshape3.png'))
+        self.images.append(pygame.image.load('Images/Tshape4.png'))
+        self.images.append(pygame.image.load('Images/Tshape5.png'))
+        self.images.append(pygame.image.load('Images/Tshape6.png'))
+        self.images.append(pygame.image.load('Images/Tshape7.png'))
+        self.images.append(pygame.image.load('Images/Tshape8.png'))
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(10, 500, 100, 100)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+class OShapeSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(OShapeSprite, self).__init__()
+        self.images = []
+        self.images.append(pygame.image.load('Images/Oshape.png'))
+        self.images.append(pygame.image.load('Images/Oshape1.png'))
+        self.images.append(pygame.image.load('Images/Oshape2.png'))
+        self.images.append(pygame.image.load('Images/Oshape3.png'))
+        self.images.append(pygame.image.load('Images/Oshape4.png'))
+        self.images.append(pygame.image.load('Images/Oshape5.png'))
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(10, 200, 100, 100)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+
 screen = pygame.display.set_mode((w_width, w_height))
 pygame.mouse.set_visible(True)
 pygame.display.set_caption('Block Game')
 program_icon = pygame.image.load('icon.png')
 pygame.display.set_icon(program_icon)
+
+my_sprite2 = LShapeSprite()
+my_group2 = pygame.sprite.Group(my_sprite2)
+my_sprite3 = OShapeSprite()
+my_group3 = pygame.sprite.Group(my_sprite3)
+my_sprite4 = TShapeSprite()
+my_group4 = pygame.sprite.Group(my_sprite4)
+my_sprite5 = ZShapeSprite()
+my_group5 = pygame.sprite.Group(my_sprite5)
+clock = pygame.time.Clock()
 
 color_speed = 1
 color_direction = [-1, 1, -1]
@@ -34,26 +138,6 @@ def color_change(color, direction):
             direction[i] *= -1
 
 def menu():
-    # main menu
-    # start game button
-    start_button = pygame.draw.rect(screen, (118, 238, 198), pygame.Rect(320, 350, 160, 100))
-
-    # quit game button
-    quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
-
-    # text on buttons
-    font = pygame.font.SysFont('franklingothicmedium', 50)
-    start_text = font.render('Start', False, BLACK)
-    start_rect = start_text.get_rect(center=start_button.center)
-    screen.blit(start_text, start_rect)
-    quit_text = font.render('Quit', False, BLACK)
-    quit_rect = quit_text.get_rect(center=quit_button.center)
-    screen.blit(quit_text, quit_rect)
-
-    # add everything to screen
-    pygame.mouse.set_visible(True)
-    pygame.display.flip()
-
     # add menu music
     # pygame.mixer.init()
     # pygame.mixer.music.load('birthofahero.wav')
@@ -74,6 +158,17 @@ def menu():
                 pygame.quit()
                 sys.exit(0)
 
+        screen.fill(BLACK)
+        my_group2.draw(screen)
+        my_group2.update()
+        my_group3.draw(screen)
+        my_group3.update()
+        my_group4.draw(screen)
+        my_group4.update()
+        my_group5.draw(screen)
+        my_group5.update()
+        clock.tick(6)
+
         # title game button & text
         title_button = pygame.draw.rect(screen, BLACK, pygame.Rect(320, 120, 160, 100))
         title_font = pygame.font.SysFont('javanesetext', 100)
@@ -90,39 +185,27 @@ def menu():
         color_change(default_color, color_direction)
         color_change(default_color1, color_direction1)
 
-        pygame.display.update()
+        # start game button
+        start_button = pygame.draw.rect(screen, (118, 238, 198), pygame.Rect(320, 350, 160, 100))
+
+        # quit game button
+        quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
+
+        # text on buttons
+        font = pygame.font.SysFont('franklingothicmedium', 50)
+        start_text = font.render('Start', False, BLACK)
+        start_rect = start_text.get_rect(center=start_button.center)
+        screen.blit(start_text, start_rect)
+        quit_text = font.render('Quit', False, BLACK)
+        quit_rect = quit_text.get_rect(center=quit_button.center)
+        screen.blit(quit_text, quit_rect)
+
+        # add everything to screen
+        pygame.mouse.set_visible(True)
+        pygame.display.flip()
 
 
 def game_select():
-    screen.fill(BLACK)
-    # main menu
-    # start game button
-
-    limit5_button = pygame.draw.rect(screen, (141, 238, 238), pygame.Rect(320, 250, 160, 100))
-    limit10_button = pygame.draw.rect(screen, (30, 144, 255), pygame.Rect(320, 350, 160, 100))
-    limit15_button = pygame.draw.rect(screen, (141, 238, 238), pygame.Rect(320, 450, 160, 100))
-
-    # quit game button
-    quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
-
-    # text on buttons
-    font = pygame.font.SysFont('franklingothicmedium', 28)
-    font_title = pygame.font.SysFont('franklingothicmedium', 60)
-
-    limit5_text = font.render('5 Min Limit', False, BLACK)
-    limit10_text = font.render('10 Min Limit', False, BLACK)
-    limit15_text = font.render('15 Min Limit', False, BLACK)
-    limit5_rect = limit5_text.get_rect(center=limit5_button.center)
-    limit10_rect = limit10_text.get_rect(center=limit10_button.center)
-    limit15_rect = limit15_text.get_rect(center=limit15_button.center)
-    screen.blit(limit5_text, limit5_rect)
-    screen.blit(limit10_text, limit10_rect)
-    screen.blit(limit15_text, limit15_rect)
-    quit_text = font.render('Quit', False, BLACK)
-    quit_rect = quit_text.get_rect(center=quit_button.center)
-    screen.blit(quit_text, quit_rect)
-    pygame.mouse.set_visible(True)
-    pygame.display.flip()
     while True:
         for event in pygame.event.get():
             # on event click
@@ -141,6 +224,48 @@ def game_select():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
+
+        screen.fill(BLACK)
+        my_group2.draw(screen)
+        my_group2.update()
+        my_group3.draw(screen)
+        my_group3.update()
+        my_group4.draw(screen)
+        my_group4.update()
+        my_group5.draw(screen)
+        my_group5.update()
+        clock.tick(6)
+
+        pygame.display.update()
+
+        # main menu
+        # start game button
+        limit5_button = pygame.draw.rect(screen, (141, 238, 238), pygame.Rect(320, 250, 160, 100))
+        limit10_button = pygame.draw.rect(screen, (30, 144, 255), pygame.Rect(320, 350, 160, 100))
+        limit15_button = pygame.draw.rect(screen, (141, 238, 238), pygame.Rect(320, 450, 160, 100))
+
+        # quit game button
+        quit_button = pygame.draw.rect(screen, RED, pygame.Rect(320, 550, 160, 100))
+
+        # text on buttons
+        font = pygame.font.SysFont('franklingothicmedium', 28)
+        font_title = pygame.font.SysFont('franklingothicmedium', 60)
+
+        limit5_text = font.render('5 Min Limit', False, BLACK)
+        limit10_text = font.render('10 Min Limit', False, BLACK)
+        limit15_text = font.render('15 Min Limit', False, BLACK)
+        limit5_rect = limit5_text.get_rect(center=limit5_button.center)
+        limit10_rect = limit10_text.get_rect(center=limit10_button.center)
+        limit15_rect = limit15_text.get_rect(center=limit15_button.center)
+        screen.blit(limit5_text, limit5_rect)
+        screen.blit(limit10_text, limit10_rect)
+        screen.blit(limit15_text, limit15_rect)
+        quit_text = font.render('Quit', False, BLACK)
+        quit_rect = quit_text.get_rect(center=quit_button.center)
+        screen.blit(quit_text, quit_rect)
+        pygame.mouse.set_visible(True)
+        pygame.display.flip()
+
 
 #UNFINISHED DIFFICULTY SELECT SCREEN
 def diff_select():
@@ -191,4 +316,3 @@ def diff_select():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
-
